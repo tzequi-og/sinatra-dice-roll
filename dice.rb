728d4fd991e117cbe8a9d33e261c1240397ac4fd
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-17 15:54:42 +0000

    improved

--- dice.rb
+++ dice.rb
@@ -2,10 +2,26 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "<h1>Dice Roll!</h1>
-  <ul>"
+  <body>
+    <div>
+      <ul>
+        <li><a href='/dice/2/6'>Roll two 6-sided dice</a></li>
+        <li><a href='/dice/2/10'>Roll two 10-sided dice</a></li>
+        <li><a href='/dice/1/20'>Roll one 20-sided dice</a></li>
+        <li><a href='/dice/5/4'>Roll five 4-sided dice</a></li>
+      </ul>
+    </div>
+  </body>"
 end
 
 get("/dice/2/6") do
